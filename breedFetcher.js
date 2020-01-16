749a47fc0e38ee4e053affd2547082297a4a64ff
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-16 21:18:58 +0000

    iimplement breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,16 @@
 const request = require('request');
+const args = process.argv.slice(2);
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) =>{
+  if (error) {
+    console.log(error);
+    return;
+  }
+  if (body === '[]') {
+    console.log('Breed not found!');
+    return;
+  }
+  const data = JSON.parse(body);
+  const description = data[0].description;
+  console.log(description);
+});
