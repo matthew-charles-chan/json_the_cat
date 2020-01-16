4bdc14c068ef969ba27df6159db71246d1e9b093
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-16 23:27:39 +0000

    implemented mocha chai testing on breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,17 +3,17 @@ const request = require('request');
 const fetchBreedDescription = (breed, cb) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) =>{
     if (error !== null) {
-      cb(error, null)
+      cb(error, null);
     }
     const data = JSON.parse(body);
     if (body === '[]') {
-      cb(null, 'Breed not found!')
-    } else { 
-      const description = data[0].description;  
-      cb(null, description)
-    }  
+      cb(null, 'Breed not found!');
+    } else {
+      const description = data[0].description;
+      cb(null, description);
+    }
   });
-}
+};
 
-module.exports = fetchBreedDescription
+module.exports = fetchBreedDescription;
 
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
 const args = process.argv.slice(2);
 
-const fetchBreedDescription = require('./breedFetcher')
+const fetchBreedDescription = require('./breedFetcher');
 
 fetchBreedDescription(args[0], (error, description) => {
   if (error) {
-    console.log('Error fetch details:', error)
+    console.log('Error fetch details:', error);
   } else {
-    console.log(description)
+    console.log(description);
   }
 });
